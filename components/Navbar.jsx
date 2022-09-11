b465b55cbe291a60995fe1f3cec684290821e62b
Biswajit <biswajitghosh150803@gmail.com>
2022-09-11 20:25:35 +0530

    Final

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,19 +10,6 @@ const Navbar = () => {
       <p className="logo">
         <Link href="/">SastaBazar</Link>
       </p>
-      <div className="nav_searchbaar">
-            <input
-            className="input"
-              type="text"
-              name=""
-              // onChange={(e) => getText(e.target.value)}
-              placeholder="Search Your Fruits here....."
-            />
-            <div className="search_icon">
-              <i className="fas fa-search" id="search"></i>
-              <AiOutlineSearch id="search" />
-            </div>
-            </div>
       <button type="button" className="cart-icon" onClick={()=>setShowCart(true)}>
         <AiOutlineShopping />
         <span className="cart-item-qty">{totalQuantities}</span>
