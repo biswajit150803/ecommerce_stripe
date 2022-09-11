54d74cb6d8432a681ff60a1410746b1f22a0126b
Biswajit <biswajitghosh150803@gmail.com>
2022-09-11 12:17:24 +0530

    Styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import Link from "next/link";
-import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+import { AiFillInstagram, AiFillMail, AiFillPhone, AiOutlineTwitter } from "react-icons/ai";
 const Footer = () => {
   return (
     <div className="footer-container">
     <Link href='/'>
-    <p style={{cursor:"pointer"}}>2022 SastaBazar Pvt. Ltd. All rights reserved</p>
+    <p style={{cursor:"pointer",fontSize:"25px"}}>2022 SastaBazar Pvt. Ltd. All rights reserved</p>
     </Link>
+      <p>Get in touch with Us....</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
+        <Link href="mailto:query@sastabazar.com" style={{cursor:"pointer"}}>
+        <AiFillMail />
+          </Link>       
       </p>
+      <p>Or call us at our tollfree nummber 1800 123 768</p> 
     </div>
   );
 };
 
 export default Footer;
+
+
+
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { AiOutlineShopping } from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineShopping } from "react-icons/ai";
 import Cart from "./Cart";
 import { useStateContext } from "../context/StateContext";
 const Navbar = () => {
@@ -10,6 +10,19 @@ const Navbar = () => {
       <p className="logo">
         <Link href="/">SastaBazar</Link>
       </p>
+      <div className="nav_searchbaar">
+            <input
+            className="input"
+              type="text"
+              name=""
+              // onChange={(e) => getText(e.target.value)}
+              placeholder="Search Your Fruits here....."
+            />
+            <div className="search_icon">
+              <i className="fas fa-search" id="search"></i>
+              <AiOutlineSearch id="search" />
+            </div>
+            </div>
       <button type="button" className="cart-icon" onClick={()=>setShowCart(true)}>
         <AiOutlineShopping />
         <span className="cart-item-qty">{totalQuantities}</span>
